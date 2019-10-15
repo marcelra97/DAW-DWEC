165b573ee10cbc2daeb1ff9eff7bc25ce7003fe9
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-10-15 20:25:34 +0200

    Calcualdora hecha

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,18 +1,12 @@
 var pantalla;
 var numeros = "";
-var signo= false;
+var signo = false;
 window.onload = function() {
 
     pantalla = document.getElementById("resultado");
     pantalla.value = "0";
 }
 
-/*window.addEventListener('keypress', function(e) {
-    var keycode = e.which || e.keyCode;
-    var valueEntered = String.fromCharCode(keycode);
-    ponerNumero(valueEntered);
-  });*/
-  
 document.addEventListener("DOMContentLoaded", function() {
 
     //me creo un array de botones donde me guardo las clases de esos botones
@@ -28,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("resetear").addEventListener("click", reset);
 
-   
+
 
 })
 
@@ -47,99 +41,86 @@ function soltarBoton() {
 
 }
 
-function ponerNumero() {
+function ponerNumero(evento) {
 
-    var caracter;
-    
-    /*si es el id eliminar le meto el id al caracter porque no tengo ni idea de como poner el signo de retroceso*/ 
-    if(this.id == "eliminar"){
+
+    var caracter = evento;
+
+    /*si es el id eliminar le meto el id al caracter porque no tengo ni idea de como poner el signo de retroceso*/
+    if (this.id == "eliminar") {
 
         caracter = this.id;
-    
-    /*si no es eliminar pues cojes el inner text de los demas*/    
-    }else{
+
+        /*si no es eliminar pues cojes el inner text de los demas*/
+    } else {
 
         caracter = this.innerText;
-        
+
     }
 
+
+
     /*Si es eliminar eliminas el ultimo numero */
-    if(caracter == "eliminar"){
+    if (caracter == "eliminar") {
 
         if (pantalla.value.length > 1) {
 
             numeros = pantalla.value.substring(0, numeros.length - 1);
-            
-                pantalla.value = numeros;
-                
-                /*Cuando borre el signo que se ponga en false porque sino pega un bugazo*/ 
-                signo=false;
-           
-           /*si borras el ultimo numero llama a reset para que ponga un cero*/      
-        }else{
+
+            pantalla.value = numeros;
+
+            /*Cuando borre el signo que se ponga en false porque sino pega un bugazo*/
+            signo = false;
+
+            /*si borras el ultimo numero llama a reset para que ponga un cero*/
+        } else {
 
             reset();
 
         }
 
-    /*Si es un igual llamas a calcular*/
-    }else if(caracter == "="){
-        
+        /*Si es un igual llamas a calcular*/
+    } else if (caracter == "=") {
+
         calcular(numeros);
 
-    /*Si no es ningun caracter especial pues se ponen los numeros en la panatalla*/    
-    }else{
-        
+        /*Si no es ningun caracter especial pues se ponen los numeros en la panatalla*/
+    } else if (caracter == "()") {
+
+        numeros = "(" + numeros + ")";
+        pantalla.value = numeros;
+
+    } else {
+
         /*Si la cadena esta vacia y es un numero pones el primer caracter */
-        if(numeros == "" && !isNaN(caracter)){
+        if (numeros == "" && !isNaN(caracter)) {
 
-             numeros = caracter;
-    
+            numeros = caracter;
+
             pantalla.value = numeros;
-        
-        /*Si el numero ya tiene un numero ya puedes poner signos*/
-        }else if(numeros.length > 0 ){
 
-            /*Cuando el caracter no sea un numero y no haya puesto ningun signo antes, puedes poner un signo*/ 
-            if(isNaN(caracter) && !signo){
+            /*Si el numero ya tiene un numero ya puedes poner signos*/
+        } else if (numeros.length > 0) {
+
+            /*Cuando el caracter no sea un numero y nconsole.log(caracter);o haya puesto ningun signo antes, puedes poner un signo*/
+            if (isNaN(caracter) && !signo) {
 
                 numeros += caracter;
-                
+
                 signo = true;
 
-            /*Cuando el caracter sea un numero, solo puedes poner un numero y ya en el siguiente te permite poner un signo*/
-            }else if(!isNaN(caracter)){
+                /*Cuando el caracter sea un numero, solo puedes poner un numero y ya en el siguiente te permite poner un signo*/
+            } else if (!isNaN(caracter)) {
 
                 numeros += caracter;
 
-                signo= false;
+                signo = false;
             }
 
             pantalla.value = numeros;
 
-        }   
+        }
     }
-
-
-    
-    //si el opreacion esta vacio y si no es un simbolo le pones el primer numero
-    /*
-    if (numeros == "" && !isNaN(digito)) {
-        
-        
-        numeros = digito;
-
-        //sino concatenas con los siguientes     
-    } else if(numeros.length > 0 ) {
-
-        numeros += digito;
-        
-    } else if(digito == "="){
-
-        calcular(numeros);
-    }
-
-    pantalla.value = numeros;*/
 
 }
 
@@ -149,15 +130,17 @@ function reset() {
     numeros = "0";
     pantalla.value = numeros;
     numeros = "";
+    signo = false;
 
 }
 
 /*Calcular el resultado */
-function calcular(resultado){
+function calcular(resultado) {
 
     resultado = resultado.replace("x", "*");
 
     pantalla.value = eval(resultado);
-    
 
+    resultado = "";
+    numeros = "";
 }
