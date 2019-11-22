6950a04ee01a5ab35708ab658b9679f664094555
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-11-22 18:51:48 +0100

    + Hecho el banner de publicidad

--- ReproductorVideo/js/reproductorscript.js
+++ ReproductorVideo/js/reproductorscript.js
@@ -2,6 +2,12 @@ function init() {
 
     video = document.querySelector("video");
 
+    let nodoVideo = document.querySelector("#multimedia");
+    let banner = document.createElement("div");
+
+    banner.classList.add("banner");
+    nodoVideo.appendChild(banner);
+
     document.querySelector("#play").addEventListener('click', play);
     document.querySelector("#silenciar").addEventListener('click', muted);
     document.querySelector("#rebobinar").addEventListener('click', rebobinar);
@@ -9,6 +15,30 @@ function init() {
     document.querySelector("#avanzar").addEventListener('click', avanzar);
     document.querySelector("#menos").addEventListener('click', volumenDown);
     document.querySelector("#mas").addEventListener('click', volumenUp);
+    apareceBanner();
+
+}
+
+function apareceBanner() {
+
+    let divBanner = document.querySelector('.banner');
+    let p = document.createElement('p');
+    let img = document.createElement('div');
+    p.innerText = "Aqui tienes una rica publicidad de gratis amejo. Disfrutala!";
+    divBanner.appendChild(p);
+    divBanner.appendChild(img);
+
+
+
+    //setTimeout(eliminarBanner, 10000);
+
+}
+
+function eliminarBanner() {
+
+    let padreBanner = document.querySelector("#multimedia");
+    let hijo = document.querySelector(".banner");
+    padreBanner.removeChild(hijo);
 }
 
 
