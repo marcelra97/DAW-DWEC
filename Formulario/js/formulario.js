b291e7be9c47354d51ac45c5d5d3fac337764352
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-12-03 20:26:23 +0100

    + Haciendo el ajax

--- Formulario/js/formulario.js
+++ Formulario/js/formulario.js
@@ -12,11 +12,6 @@
         document.querySelector("[name=ip]").addEventListener('blur', comprobarIp);
         document.querySelector("button").addEventListener('click', validarFormulario);
 
-        ruta = "http://localhost/formulario.php";
-        metodo = "GET";
-        envio = null;
-        xml = true;
-
 
     }
 
@@ -182,7 +177,7 @@
 
         if (comprobarCorrecto == 7) {
 
-            alert("Se han introducido todos los datos correctamente");
+            consultar();
 
         } else {
 
@@ -200,9 +195,42 @@
 
     }
 
+    function consultar() {
+
+        ruta = 'http://localhost/formulario.php?' +
+            'nombre=' + dato[0] + ' ';
+        metodo = "GET";
+        envio = null;
+        xml = true;
+
+        descargar(metodo, ruta, envio, xml, tratarXML);
+    }
+
+    function descargar(metodo, ruta, envio, xml, funcion) {
+        // Obtener la instancia del objeto XMLHttpRequest
+        let peticion_http = new XMLHttpRequest();
+
+        if (xml) peticion_http.overrideMimeType('text/xml');
+        // Preparar la función de respuesta
+        peticion_http.onreadystatechange = mostrar;
+        // Realizar petición HTTP
+        peticion_http.open(metodo, ruta, true);
+        peticion_http.send(envio);
+
+        function mostrar() {
+            if (peticion_http.readyState == 4 && peticion_http.status == 200) {
+
+                if (xml) funcion(peticion_http.responseXML);
+
+                else funcion(peticion_http.responseText);
+            }
+        }
+    }
+
     let password;
     let ruta;
     let metodo;
     let envio;
     let xml;
+    let dato;
     window.onload = init;
